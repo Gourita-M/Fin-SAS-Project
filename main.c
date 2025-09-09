2f621e1daeef17465ae188ebf863d0d643677223
Gourita-M <mouad.gourita@gmail.com>
2025-09-09 17:45:20 +0100

    finished the main

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(){
+     int choice;
+
+    do{
+    
+      printf("\n-----------Football Team Managment-----------");
+      printf("\n1. Add a Player");
+      printf("\n2. Show All Players");
+      printf("\n3. Edit Player");
+      printf("\n4. Delete Player");
+      printf("\n5. Search for Player");
+      printf("\n6. Statistics");
+      printf("\n0. Exit");
+      printf("\n---------------------------------------------");
+      printf("\nEnter Your Choice: ");
+           scanf("%d",&choice);
+       switch (choice){
+       case 1:
+        
+        break;
+       case 2:
+        
+        break;
+       case 3:
+        
+        break;
+       case 4:
+        
+        break;
+       case 5:
+        
+        break;
+       case 6:
+        
+        break;
+       
+       default:
+        break;
+       }
+
+
+    } while (choice != 0);
+    return 0;
+}

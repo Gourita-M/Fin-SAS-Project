454c97a4e4468bef32370876a4d47978df077e2c
Gourita-M <mouad.gourita@gmail.com>
2025-09-09 17:59:51 +0100

    adding a player code

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct{
+       int ID;
+       char name[100][15];
+       char prenome[100][15];
+       int playernumber;
+       char playerrole[100][15];
+       int age;
+       int goals;
+}player;
+player p1;
+
+void Addingplayer();
+
 
 int main(){
      int choice;
@@ -19,8 +32,8 @@ int main(){
       printf("\nEnter Your Choice: ");
            scanf("%d",&choice);
        switch (choice){
-       case 1:
-        
+       case 1:  // Add a Player
+              Addingplayer();
         break;
        case 2:
         
@@ -45,4 +58,28 @@ int main(){
 
     } while (choice != 0);
     return 0;
+}
+
+void Addingplayer(){
+       int choice1;
+         
+  
+      printf("\n-----------Football Team Managment-----------");
+      printf("\n1. Add a New Player");
+      printf("\n2. Add multiple players");
+      printf("\n---------------------------------------------");
+      printf("\nEnter Your Choice: ");
+           scanf("%d",&choice1); 
+
+             if(choice1 == 1){
+
+
+             } else if(choice1 == 2){
+
+             }else{
+                   printf("wrong Choice must be ( 1 or 2)");
+                   Addingplayer();
+             }
+  
+
 }
